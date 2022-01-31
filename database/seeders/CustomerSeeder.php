10974ecdc33d1845c7c0e532f51557c539f93869
Rabi Kr Yadav <rabikrishnayadav@gmail.com>
2022-01-31 13:43:29 +0545

    data add using faker and seeder in customer table

--- database/seeders/CustomerSeeder.php
+++ database/seeders/CustomerSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Customer;
+use Faker\Factory as Faker;
 class CustomerSeeder extends Seeder
 {
     /**
@@ -13,16 +14,17 @@ class CustomerSeeder extends Seeder
      */
     public function run()
     {
-        // seeder use for add data in table
+        // faker for add many data in database
+        $faker = Faker::create();
         $customer = new Customer;
-        $customer->user_name = "Rabi Kr Yadav";
-        $customer->email = "mail@rabi.com";
-        $customer->password = '123';
-        $customer->gender = 'M';
-        $customer->address = 'bangalore';
-        $customer->state = 'karnatka';
-        $customer->country = 'india';
-        $customer->dob = now();
+        $customer->user_name = $faker->name;
+        $customer->email = $faker->email;
+        $customer->password = $faker->password;
+        $customer->gender = 'O';
+        $customer->address = $faker->address;
+        $customer->state = $faker->state;
+        $customer->country = $faker->country;
+        $customer->dob = $faker->date;
         $customer->save();
         // return redirect('/customer/view');
     }
